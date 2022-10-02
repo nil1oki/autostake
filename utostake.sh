1179467050285df490fd7f03f8e8fd900fc7a388
Ruslan <102181017+nil1oki@users.noreply.github.com>
2022-10-02 20:51:14 +0600

    Update utostake.sh

--- utostake.sh
+++ utostake.sh
@@ -20,7 +20,7 @@ for (( ;; )); do
         done
  
         BAL=$(dewebd q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount')
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} utollo\n"
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} udws\n"
 
         echo -e "Claim rewards\n"
         echo -e "${PWD}\n${PWD}\n" | dewebd tx distribution withdraw-all-rewards --from ${ACC_NAME} --chain-id ${CHAIN_NAME} --gas=auto --fees=1000udws --node ${NODE} --yes
@@ -31,12 +31,12 @@ for (( ;; )); do
         done
         BAL=$(dewebd q bank balances ${DELEGATOR_ADDRESS} --node ${NODE} -o json | jq -r '.balances | .[].amount');
         BAL=$(($BAL-1000000))
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} utollo\n"
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} udws\n"
         echo -e "Stake ALL\n"
         if (( BAL > 1000000 )); then
         echo -e "${PWD}\n${PWD}\n" | dewebd tx staking delegate ${VALIDATOR_ADDRESS} ${BAL}udws --from ${ACC_NAME} --gas=auto --fees=1000udws --node ${NODE} --chain-id ${CHAIN_NAME}  --yes
         else
-        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} utollo BAL < 10000000 ((((\n"
+        echo -e "BALANCE: ${GREEN_COLOR}${BAL}${WITHOU_COLOR} udws BAL < 10000000 ((((\n"
         fi
         for (( timer=${DELAY}; timer>0; timer-- ))
         do
